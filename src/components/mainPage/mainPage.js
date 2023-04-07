99f4ef8eb99e1be701c9745eed6d09e6e79282f3
Qanpi <aleksei.terin@outlook.com>
2023-04-07 20:34:00 +0300

    refactored linking mechanism

--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -1,7 +1,69 @@
 import React, { useEffect, useRef, useState } from "react";
 import { TypeAnimation } from "react-type-animation";
 import Editor from "../editor/editor";
+import GithubPage from "../githubPage/githubPage";
 import "./mainPage.css";
+
+function Nordea() {
+  return (
+    <Editor className="experience">
+      <p className="green">Sep 1, 2021.</p>
+      <p>A week-long experience at Nordea for TET.</p>
+      <p></p>
+      <p>
+        As part of my responsibilities, created material about entrepreneurship.
+      </p>
+      <p></p>
+      <p>
+        Also appearead on{" "}
+        <span className="emphasis">national TV and Nordea social media.</span>
+      </p>
+      <p></p>
+      <p>Plus, met and talked to some great people.</p>
+    </Editor>
+  );
+}
+
+function Slush() {
+  return (
+    <Editor>
+      <p className="green">Nov 2022.</p>
+      <p>
+        Attended as a{" "}
+        <span className="emphasis">volunteer in Partnership Chambers.</span>
+      </p>
+      <p></p>
+      <p>Got to meet great people, and great companies.</p>
+      <p>+ great merch :)</p>
+      <p></p>
+      <p>Planning to apply for position as Group Lead next season.</p>
+    </Editor>
+  );
+}
+function Rovio() {
+  return (
+    <Editor>
+      <p className="green">January 2023.</p>
+      <p>
+        After winning the{" "}
+        <span className="emphasis">
+          first place in a Rovio-sponsored hackathon
+        </span>
+        , my friends and I were awarded a day at the office.
+      </p>
+      <p></p>
+      <p>
+        We got a tour of the Rovio building, insight into unreleased games and a
+        bunch of snacks.
+      </p>
+      <p></p>
+      <p>
+        <span className="emphasis">The people at Rovio</span>, and the
+        discussions we shared, were undoubtedly the best part of it, though.
+      </p>
+    </Editor>
+  );
+}
 
 const useContainerDimensions = (myRef) => {
   const [dimensions, setDimensions] = useState({
@@ -56,6 +118,7 @@ function TypeAnimationWrapper({ normalText, sequence }) {
   );
 }
 
+
 export default function MainPage(onLinkClick) {
   const interests = [
     "React",
@@ -78,19 +141,19 @@ export default function MainPage(onLinkClick) {
       <p></p>
 
       <p className="folder">portfolio/</p>
-      <p className="file" onClick={() => onLinkClick("iqArrowsSolver.java")}>
+      <p className="file" onClick={() => onLinkClick("iqArrowsSolver.java", GithubPage("iqArrowsSolver"))}>
         iqArrowsSolver<span className="green">.java</span>
       </p>
-      <p className="file" onClick={() => onLinkClick("aoc2022.clj")}>
+      <p className="file" onClick={() => onLinkClick("aoc2022.clj", GithubPage("aoc2022"))}>
         aoc2022<span className="green">.clj</span>
       </p>
-      <p className="file" onClick={() => onLinkClick("stega.py")}>
+      <p className="file" onClick={() => onLinkClick("stega.py", GithubPage("stega"))}>
         stega<span className="green">.py</span>
       </p>
-      <p className="file" onClick={() => onLinkClick("inception.java")}>
+      <p className="file" onClick={() => onLinkClick("inception.java", GithubPage("inception"))}>
         inception<span className="green">.java</span>
       </p>
-      <p className="file" onClick={() => onLinkClick("datatähti2023.cpp")}>
+      <p className="file" onClick={() => onLinkClick("datatähti2023.cpp", GithubPage("datatahti2023"))}>
         datatähti2023<span className="green">.cpp</span>
       </p>
   
@@ -105,9 +168,9 @@ export default function MainPage(onLinkClick) {
 
       <p></p>
       <p className="folder">experience/</p>
-      <p className="file" onClick={() => onLinkClick("nordea")}>nordea</p>
-      <p className="file" onClick={() => onLinkClick("slush")}>slush2022</p>
-      <p className="file" onClick={() => onLinkClick("rovio")}>rovio</p>
+      <p className="file" onClick={() => onLinkClick("nordea", Nordea())}>nordea</p>
+      <p className="file" onClick={() => onLinkClick("slush", Slush())}>slush2022</p>
+      <p className="file" onClick={() => onLinkClick("rovio", Rovio())}>rovio</p>
 
       <p></p>
       <p></p>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useLayoutEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
@@ -8,149 +8,119 @@ import Editor from "./components/editor/editor";
 
 function TabBar({ titles, selected, onTabOpen, onTabClose }) {
   const tabs = titles.map((t, i) => (
-    <div key={t} className="tab" id={selected === t ? "selected" : ""} onClick={() => onTabOpen(t)}>
+    <div
+      key={t}
+      className={"tab" + (t === "main" ? " main-tab" : "")}
+      id={selected === t ? "selected" : ""}
+      onClick={() => onTabOpen(t, null)}
+    >
       <span>{t}</span>
-      <button onClick={ev => onTabClose(ev, t)}>✕</button>
+      <button onClick={(ev) => onTabClose(ev, t)}>✕</button>
     </div>
   ));
 
   return <div className="tab-bar">{tabs}</div>;
 }
 
-function Nordea() {
-  return (
-    <Editor className="experience">
-      <p className="green">Sep 1, 2021.</p>
-      <p>A week-long experience at Nordea for TET.</p>
-      <p></p>
-      <p>As part of my responsibilities, created material about entrepreneurship.</p>
-      <p></p>
-      <p>Also appearead on <span className="emphasis">national TV and Nordea social media.</span></p>
-      <p></p>
-      <p>Plus, met and talked to some great people.</p>
-    </Editor>
-  )
-}
-
-function Slush() {
-  return (
-    <Editor>
-      <p className="green">Nov 2022.</p>
-      <p>Attended as a <span className="emphasis">volunteer in Partnership Chambers.</span></p>
-      <p></p>
-      <p>Got to meet great people, and great companies.</p>
-      <p>+ great merch :)</p>
-      <p></p>
-      <p>Planning to apply for position as Group Lead next season.</p>
-    </Editor>
-  )
-}
-
-function Rovio() {
-  return (
-    <Editor>
-      <p className="green">January 2023.</p>
-      <p>After winning the <span className="emphasis">first place in a Rovio-sponsored hackathon</span>, my friends and I were awarded a day at the office.</p>
-      <p></p>
-      <p>We got a tour of the Rovio building, insight into unreleased games and a bunch of snacks.</p>
-      <p></p>
-      <p><span className="emphasis">The people at Rovio</span>, and the discussions we shared, were undoubtedly the best part of it, though.</p>
-    </Editor>
-  )
-}
-
-function Footer({filename}) {
-  const [scrollY, setScrollY] = useState(0)
+function Footer({ filename, pageRef }) {
+  const [scrollY, setScrollY] = useState(0);
   const [pageHeight, setPageHeight] = useState(1);
-  const percentage = Math.round(scrollY / pageHeight * 100);
+  const percentage = Math.round((scrollY / pageHeight) * 100);
 
   useEffect(() => {
     function handleScroll(ev) {
-      const doc = ev.target.documentElement;
+      const el = ev.target;
 
-      setScrollY(doc.scrollTop);
-      setPageHeight(doc.offsetHeight - doc.clientHeight);
+      setScrollY(el.scrollTop);
+      setPageHeight(el.scrollTopMax);
     }
 
-    window.addEventListener("scroll", handleScroll)
+    pageRef.current.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  })
+    return () => pageRef.current.removeEventListener("scroll", handleScroll);
+  });
 
-   return (
-<div className="footer">
-        <div className="left">
-          <div className="mode-indicator"><span>NORMAL</span></div>
-          <span>{filename}</span>
+  return (
+    <div className="footer">
+      <div className="left">
+        <div className="mode-indicator">
+          <span>NORMAL</span>
         </div>
-        <div className="right">
-          <span>unix | netrw</span>
-          <span >{percentage}%</span>
-        </div>
+        <span>{filename}</span>
       </div>
-  )
+      <div className="right">
+        <span>unix | netrw</span>
+        <span>{percentage}%</span>
+      </div>
+    </div>
+  );
 }
 
 function Window() {
-  //too many magic strings, maybe address somehow later
-  const pagesLookup = {
-    "main": MainPage(handleLinkClick),
-    //portfolio
-    "iqArrowsSolver.java": GithubPage("iqArrowsSolver"),
-    "aoc2022.clj": GithubPage("aoc2022"),
-    "inception.java": GithubPage("inception"),
-    "datatähti2023.cpp": GithubPage("datatahti2023"),
-    "stega.py": GithubPage("stega"),
-    //experience
-    "nordea": Nordea(),
-    "slush": Slush(), 
-    "rovio": Rovio()
+  const [openPages, setOpenPages] = useState({
+    main: { page: MainPage(handleTabOpen), y: 0 },
+  });
+  const [history, setHistory] = useState(new Set().add("main"));
+
+  const titles = Object.keys(openPages);
+  const tab = Array.from(history).pop();
+  const page = openPages[tab]["page"];
+
+  const pageRef = useRef(null);
+
+  useLayoutEffect(() => {
+    if (pageRef.current) {
+      const weirdOffset = tab === "main" ? 72 : 0; //very botched fix for some autoscrolling weirdness 
+      pageRef.current.scrollTo(0, openPages[tab]["y"] - weirdOffset);
+    }
+  }, [tab]);
+
+  function handleTabOpen(title, page) {
+    setOpenPages((prevState) => {
+      console.log(tab, pageRef.current.scrollTop);
+      prevState[tab]["y"] = pageRef.current.scrollTop;
+      if (title in prevState) return prevState;
+      else return { ...prevState, [title]: { page: page, y: 0 } };
+    });
+
+    setHistory((prevState) => {
+      const newState = new Set(prevState);
+      newState.delete(title);
+      return newState.add(title);
+    });
   }
 
-  const [currentTab, setCurrentTab] = useState("main");
-  const [tabs, setTabs] = useState([currentTab]);
-  const currentPage = pagesLookup[currentTab];
+  function handleTabClose(ev, title) {
+    ev.stopPropagation(); //prevent handleTabOpen from firing simultaneously
 
-  const overflow = pagesLookup[currentTab].type === "iframe" ? "hidden" : "";
+    setOpenPages((prevState) => {
+      delete prevState[title];
+      return prevState;
+    });
 
-  useEffect(() => {
-    window.document.body.style["overflow"] = overflow;
-
-    return () => window.document.body.style["overflow"] = overflow;
-  }, [overflow])
-
-
-  function handleLinkClick(address) {
-    if (!(address in pagesLookup)) return;
-    window.scroll(0, 0); 
-
-    setCurrentTab(address);
-
-    if (tabs.includes(address)) return;
-
-    const nextTabs = tabsOpen => [...tabsOpen, address];
-    setTabs(nextTabs);
-  }
-
-  function handleTabClose(ev, address) {
-    ev.stopPropagation(); //prevent handleLinkClick from firing simultaneously
+    setHistory((prevState) => {
+      const newState = new Set(prevState);
+      newState.delete(title);
+      return newState;
+    });
 
     //TODO: add rickroll
-    const i = tabs.indexOf(address);
-    const index = i > 0 ? i - 1 : 1;
-    setCurrentTab(tabs[index])
-
-    const nextTabs = tabs => tabs.filter(t => t !== address)
-    setTabs(nextTabs); 
   }
-  
+
   return (
     <>
-      <TabBar titles={tabs} selected={currentTab} onTabOpen={handleLinkClick} onTabClose={handleTabClose}/>
+      <TabBar
+        titles={titles}
+        selected={tab}
+        onTabOpen={handleTabOpen}
+        onTabClose={handleTabClose}
+      />
 
-      {currentPage} 
+      <div className="page" ref={pageRef}>
+        {page}
+      </div>
 
-      <Footer filename={currentTab}/>
+      <Footer filename={tab} pageRef={pageRef} />
     </>
   );
 }
